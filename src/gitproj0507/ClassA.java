211fd4ce33a50376458b423e4a93688915c6459a
manimekali <manimekalaij121998@gmail.com>
2024-07-05 20:45:36 +0530

    my test

---
+++ src/gitproj0507/ClassA.java
@@ -0,0 +1,22 @@
+package gitproj0507;
+
+public class ClassA {
+	 private void empid() {
+		 System.out.println("employee id is:5678");
+	 }
+	 private void empname() {
+		 System.out.println("employee name is:hayanthika k.m");
+		}
+	 private void empdesignation() {
+		System.out.println("employee designation is:software tester");
+
+	}
+	 public static void main(String[] args) {
+		 ClassA a=new ClassA();
+		 a.empid();
+		 a.empname();
+		 a.empdesignation();
+		
+	}
+
+}
